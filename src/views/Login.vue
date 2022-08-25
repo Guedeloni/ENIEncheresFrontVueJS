95a1a77d95076d0d4ded320b4a67fd2670a2b030
Thomas Chalanson <thomas.chalanson@sfr.fr>
2022-08-25 19:13:53 +0200

    do a v-if in the nav to display Admin page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -30,33 +30,17 @@ async function login() {
 <template>
     <div class="container-md">
 
-        <div><span>{{messageConfirmation}}</span></div>
-        <div><span>{{pseudo}}</span></div>
-
         <form method="post" action="">
             <div class="form-group w-25">
                 <label for="pseudo">Identifiant</label>
-                <input 
-                    type="text" 
-                    class="form-control" 
-                    id="pseudo" 
-                    name="pseudoInput" 
-                    aria-describedby="pseudo"
-                    placeholder="NineJea" 
-                    v-model="pseudo"
-                >
+                <input type="text" class="form-control" id="pseudo" name="pseudoInput" aria-describedby="pseudo"
+                    placeholder="NineJea" v-model="pseudo">
             </div>
 
             <div class="form-group w-25 mt-3">
                 <label for="password">Password</label>
-                <input 
-                    type="password" 
-                    class="form-control" 
-                    id="motDePasse" 
-                    name="mdpInput" 
-                    placeholder="Password"
-                    v-model="password"
-                >
+                <input type="password" class="form-control" id="motDePasse" name="mdpInput" placeholder="Password"
+                    v-model="password">
             </div>
 
             <div class="d-flex justify-content-around w-25 mt-3">
@@ -84,12 +68,11 @@ async function login() {
             <a class="btn btn-primary mt-3" href="/inscription"> Créer un compte </a>
         </div>
 
-        <div class="d-flex justify-content-around text-danger f-5 fw-bold w-50 my-3">
-            <!-- <c:if test="${ requestScope.message != '' }">
-                <p>${ requestScope.message }</p>
-            </c:if> -->
+        <div class="d-flex justify-content-around text-success f-5 fw-bold w-50 my-3">
+            {{ messageConfirmation }}
         </div>
     </div>
+
 </template>
 
 <style scoped>
